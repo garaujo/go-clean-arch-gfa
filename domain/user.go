bbd0c4b4520f35777801ee286337dca92f049d30
Gonçalo Araújo <garaujo@sapo.pt>
2020-09-27 21:42:47 +0100

    generated mocks added and updated

--- domain/user.go
+++ domain/user.go
@@ -13,8 +13,18 @@ type User struct {
 	DeletedAt time.Time `json:"deleted_at"`
 }
 
-// UserUsecase interface represents the user's usecases
+// UserUsecase interface represents the user's use cases contract
 type UserUsecase interface {
+	Fetch() ([]User, error)
+	Store(u *User) error
+	Update(u *User) error
+	Delete(id int64) error
+	GetByID(id int64) (User, error)
+	GetByName(n string) (User, error)
+}
+
+// UserRepository interface represents the user's repository contract
+type UserRepository interface {
 	Fetch() ([]User, error)
 	Store(u *User) error
 	Update(u *User) error
